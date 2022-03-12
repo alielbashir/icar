e01a4a87ed46d660702a8ccd2c64c90b46e99e19
Ali Elbashir <alielbashiir@gmail.com>
2022-03-12 21:07:36 +0300

    Add google map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
+
+
 
 function App() {
+
+  const location = {
+    address: 'Stockholm, Sweden',
+    lat: 59.315743484542196,
+    lng: 18.208967997981468,
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map location={location} zoomLevel={11} />
     </div>
   );
 }
