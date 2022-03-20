96f53ca6fea64e7befac6023ab3f5d0252f555a3
Ali Elbashir <alielbashiir@gmail.com>
2022-03-20 17:14:39 +0000

    Clean up cars.js

--- backend/routes/cars.js
+++ backend/routes/cars.js
@@ -1,36 +1,27 @@
 var express = require("express");
 var router = express.Router();
-var verifyToken = require('./auth');
+var verifyToken = require("./auth");
 const databaseClient = require("/workspace/backend/database");
 
 
-router.get("/", function (req, res, next) {
-   
-  });
-
-
-
-router.post("/", verifyToken, async (req, res,next) => {
-    const user_id = req.user
-    console.log(user_id)
-    // getting user's cars 
+router.get("/", verifyToken, async (req, res) => {
+  const user_id = req.user;
+  console.log(user_id);
+  // getting user's cars
   const data = await databaseClient.query(
-      `SELECT * FROM cars WHERE user_id = $1`,
-      [user_id]
-    );
+    `SELECT * FROM cars WHERE user_id = $1`,
+    [user_id]
+  );
 
-    carsData = data.rows
-    var car_ids = [];
-    for (var i = 0 ; i< carsData.length;i++) {
-       car_ids.push(carsData[i].car_id)
-    }
+  carsData = data.rows;
+  var car_ids = [];
+  for (var i = 0; i < carsData.length; i++) {
+    car_ids.push(carsData[i].car_id);
+  }
 
-    res.status(200).json({
-      car_ids: car_ids,
-    });
+  res.status(200).json({
+    car_ids: car_ids,
   });
+});
 
-
-  module.exports = router;
-
-  
+module.exports = router;
