c615be2cfafa10335edfd3ada385a4f84bcf96e7
Ali Elbashir <alielbashiir@gmail.com>
2022-03-20 20:46:51 +0000

    Fix cars not showing on sidebar

--- frontend/src/components/Sidebar/CarsList/CarsList.tsx
+++ frontend/src/components/Sidebar/CarsList/CarsList.tsx
@@ -1,13 +1,24 @@
 import { Stack } from "@fluentui/react";
 import { Cars } from "../../App/App.types";
+import CarsListItem from "./CarsListItem";
 // import CarsListItem from "./CarsListItem";
 
 const CarsList = ({ cars }: { cars: Cars }) => {
+  const filteredCars: number[] = [];
   return (
     <Stack>
-      {/* {cars.map((car) => (
-        <CarsListItem car={car} key={car.car_id} />
-      ))} */}
+      {cars
+        .filter((car) => {
+          if (filteredCars.includes(car.car_id)) {
+            return false;
+          } else {
+            filteredCars.push(car.car_id);
+            return true;
+          }
+        })
+        .map((car) => {
+          return <CarsListItem car={car} key={car.car_id} />;
+        })}
     </Stack>
   );
 };
--- frontend/src/components/Sidebar/CarsList/CarsListItem.tsx
+++ frontend/src/components/Sidebar/CarsList/CarsListItem.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Car } from "../../App/App.types";
+import { Text } from "@fluentui/react";
+
+const CarsListItem = (props: { car: Car }) => {
+  const { car } = props;
+  return (
+    <Text>
+      Car number {car.car_id} lat: {car.lat.toFixed(6)}, lng:{" "}
+      {car.lng.toFixed(6)}
+    </Text>
+  );
+};
+
+export default CarsListItem;
