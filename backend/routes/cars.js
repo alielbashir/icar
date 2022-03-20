88addcac2022acbc22760701309c69ea6a8ed71e
Ali Elbashir <alielbashiir@gmail.com>
2022-03-20 17:36:53 +0000

    Add logging for get cars endpoint

--- backend/routes/cars.js
+++ backend/routes/cars.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var verifyToken = require("./auth");
 const databaseClient = require("/workspace/backend/database");
 
-
 router.get("/", verifyToken, async (req, res) => {
   const user_id = req.user;
   console.log(user_id);
@@ -14,10 +13,14 @@ router.get("/", verifyToken, async (req, res) => {
   );
 
   carsData = data.rows;
+
+  console.log(`received ${carsData}`);
   var car_ids = [];
   for (var i = 0; i < carsData.length; i++) {
     car_ids.push(carsData[i].car_id);
   }
+
+  console.log(`car IDS for user ${user_id} = ${car_ids.toString()}`);
 
   res.status(200).json({
     car_ids: car_ids,
