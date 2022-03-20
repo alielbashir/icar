8d888513d67b23b6d654333ac9b45ea64845f817
Ali Elbashir <alielbashiir@gmail.com>
2022-03-20 17:36:38 +0000

    Change  jwt to access from authorization header

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['token'];
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
